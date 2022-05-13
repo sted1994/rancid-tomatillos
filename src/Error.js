36e8f94e5a63361db774d7addc181e6c4ccb61f8
Michael Putnam <mikeputnam713@gmail.com>
2022-05-13 16:26:00 -0700

    Add api call functionality and error messaging

---
+++ src/Error.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/error.css';
+
+const Error = () => {
+	return(
+			<div className='errors' >
+				<img src={require('./images/computer.png')} className='sadComputer' />
+				<p>We are sorry, but we could not process your request!<br/>
+				Please try refreshing the page.</p>	
+			</div>
+	)
+}
+
+export default Error; 

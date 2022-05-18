14b5e3542468fe35c1fcdd4191e4e7d8e5427e5e
Michael Putnam <mikeputnam713@gmail.com>
2022-05-17 18:18:44 -0700

    Add home page carousel testing and movie fixture

--- cypress/integration/homePage/home_page_carousel_spec.js
+++ cypress/integration/homePage/home_page_carousel_spec.js
@@ -1,5 +1,6 @@
-describe("HomePage", () => {
+describe("Home Page Carousel", () => {
   beforeEach(() => {
+    cy.intercept("GET", 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', { fixture: 'movies' })
     cy.visit('http://localhost:3000')
   });
 
@@ -16,11 +17,13 @@ describe("HomePage", () => {
   });
 
   it('Should be able to click movie in carousel to see summary', () => {
-    cy.get('.selected').children('a').children('.slide').children('.carousel-card-img').click()
+    cy.get('.carousel-display').click()
+    cy.url().should('eq', 'http://localhost:3000/659991')
+  });
 
+  it('Should be able to click control dots', () =>{
+    cy.get('.control-dots').children().last().click()
+    cy.get('.control-dots').children().first().click()
+    cy.get('.selected').children('a').children('.slide').should('have.id', 694919)
   })
-
-  // it('Should be able to click control dots', () =>{
-  //   cy.get('.control-dots')
-  // })
 })

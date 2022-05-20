db396aca051e5e2148a3fc57149a89f9b12ded6c
Stephen Tedesco <s_tylert@icloud.com>
2022-05-20 14:13:36 -0600

    Enables deploylink for GH-pages

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <Router basename={process.env.PUBLIC_URL}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
